8aba69624d0719a9912302c492e05d1988bd1c31
Johannes H. Jensen <joh@pseudoberries.com>
2011-01-03 04:43:02 +0100

    Add licensing info to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name='flickrsaver',
     version='0.1',
     description='A screensaver for Flickr enthusiasts',
+    url='http://github.com/joh/Flickrsaver',
+    license='BSD',
     author='Johannes H. Jensen',
     author_email='joh@pseudoberries.com',
-    url='http://github.com/joh/Flickrsaver',
+    
     requires=[
         'clutter (>=1.0.3)',
         'flickrapi'
